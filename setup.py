3efccaba964c57555e7f0b769f32b32fac8b4f23
William Bert <william@spanishdict.com>
2017-10-30 15:27:08 -0700

    Fix setup.py.

--- setup.py
+++ setup.py
@@ -3,20 +3,20 @@
 from setuptools import setup, find_packages
 
 
-with open('README.rst') as f:
+with open('README.md') as f:
     readme = f.read()
 
 with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='fold-to-ascii',
+    name='fold_to_ascii',
     version='1.0.0',
-    description='A Python port of the Apache Lucene ASCII Folding Filter that converts alphabetic, numeric, and symbolic Unicode characters which are not in the first 127 ASCII characters (the ‘Basic Latin’ Unicode block) into a ASCII equivalents, if they exist.',
+    description='A Python port of the Apache Lucene ASCII Folding Filter that converts alphabetic, numeric, and symbolic Unicode characters which are not in the first 127 ASCII characters (the ‘Basic Latin’ Unicode block) into ASCII equivalents, if they exist.',
     long_description=readme,
     author='William Bert',
     author_email='william@spanishdict.com',
-    url='https://github.com/spanishdict/fold-to-ascii',
+    url='https://github.com/spanishdict/fold_to_ascii',
     license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
+    packages=find_packages(exclude=('tests'))
 )
