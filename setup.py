c66537242ade0f361f950a0dc2aa207e7b57fbdb
William Bert <william@spanishdict.com>
2020-05-03 14:08:08 -0400

    Fix too-long license. (ref https://github.com/pypa/twine/issues/454)

--- setup.py
+++ setup.py
@@ -1,15 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
+import setuptools
 
 
 with open('README.md') as f:
     readme = f.read()
 
-with open('LICENSE') as f:
-    license = f.read()
-
-setup(
+setuptools.setup(
     name='fold_to_ascii',
     version='1.0.2',
     description='A Python port of the Apache Lucene ASCII Folding Filter that converts alphabetic, numeric, and symbolic Unicode characters which are not in the first 127 ASCII characters (the ‘Basic Latin’ Unicode block) into ASCII equivalents, if they exist.',
@@ -18,7 +15,7 @@ setup(
     author='William Bert',
     author_email='william@spanishdict.com',
     url='https://github.com/spanishdict/fold_to_ascii',
-    license=license,
-    packages=find_packages(exclude=('tests')),
+    license="MIT License",
+    packages=setuptools.find_packages(exclude=('tests')),
     keywords="ascii unicode sanitize diacritics fold folding ligatures"
 )
