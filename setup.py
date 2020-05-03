38116e73e383ddde4341fe61a76674edfb06dbfa
William Bert <william@spanishdict.com>
2020-05-03 13:49:13 -0400

    Long description content type.

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
     version='1.0.1',
     description='A Python port of the Apache Lucene ASCII Folding Filter that converts alphabetic, numeric, and symbolic Unicode characters which are not in the first 127 ASCII characters (the ‘Basic Latin’ Unicode block) into ASCII equivalents, if they exist.',
     long_description=readme,
+    long_description_content_type="text/markdown",
     author='William Bert',
     author_email='william@spanishdict.com',
     url='https://github.com/spanishdict/fold_to_ascii',
